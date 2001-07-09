80bf2ee4d6f1395c087ae99d483867ae92fc16e6
Lars J. Aas <larsa@sim.no>
2001-07-09 10:10:12 +0000

    Filter the cvs log correctly.

--- ChangeLog.sh
+++ ChangeLog.sh
@@ -18,10 +18,8 @@ This is the ChangeLog file for the $module module.  This file is automatically
 generated every night.  Entries are in reversed chronological order.  Check
 also out the following ChangeLog files:
 
-  ./conf-macros/ChangeLog
-  ./src/Inventor/Xt/common/ChangeLog
-  ./data/ChangeLog
-  ./examples/ChangeLog
+  ./cfg/m4/ChangeLog
+  ./src/Inventor/Win/common/ChangeLog
 
 See http://www.red-bean.com/~kfogel/cvs2cl.shtml for information about the
 cvs2cl script used to generate this file.
@@ -38,9 +36,9 @@ cvs log | $cvs2cl --stdin --header $headerfile --separate-header --prune \
   --ignore '(Makefile\.in|configure|aclocal\.m4|config\.sub|config\.guess)$' \
   --ignore '(ltconfig|ltmain\.sh|missing|mkinstalldirs|stamp-h.*|install-sh)$' \
   --ignore 'config\.h\.in$' \
-  --ignore 'conf-macros' \
+  --ignore 'cfg/m4' \
+  --ignore 'src/Inventor/Win/common' \
   --ignore 'data' \
-  --ignore 'src/Inventor/Xt/common' \
   --ignore 'examples' 
 
 rm ChangeLog.bak $headerfile
