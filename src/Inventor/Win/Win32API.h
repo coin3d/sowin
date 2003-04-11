c912a23394555579fc5196855163f0d40b79de84
Morten Eriksen <mortene@sim.no>
2003-04-11 08:47:34 +0000

    Copyright header updates, Free Software license change.

--- src/Inventor/Win/Win32API.h
+++ src/Inventor/Win/Win32API.h
@@ -1,21 +1,25 @@
-/**************************************************************************
+/**************************************************************************\
  *
- *  This file is part of the Coin SoWin GUI binding library.
- *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *  This file is part of the Coin family of 3D visualization libraries.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  version 2.1 as published by the Free Software Foundation.  See the
- *  file LICENSE.LGPL at the root directory of the distribution for
- *  more details.
+ *  This library is free software; you can redistribute it and / or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.  See the
+ *  file LICENSE.GPL at the root directory of this source distribution
+ *  for more details.
  *
- *  If you want to use Coin SoWin for applications not compatible with the
- *  LGPL, please contact SIM to aquire a Professional Edition License.
+ *  If you desire to use this library in software that is incompatible
+ *  with the GNU GPL, and / or you would like to take advantage of the
+ *  additional benefits with regard to our support services, please
+ *  contact Systems in Motion about acquiring a Coin Professional
+ *  Edition License.  See <URL:http://www.coin3d.org> for more
+ *  information.
  *
- *  Systems in Motion, Prof Brochs gate 6, N-7030 Trondheim, NORWAY
- *  http://www.sim.no/ support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *  Systems in Motion, Abels gate 5, Teknobyen, 7030 Trondheim, NORWAY
+ *  <URL:http://www.sim.no>, <mailto:support@sim.no>
  *
- **************************************************************************/
+\**************************************************************************/
 
 #ifndef SOWIN_WIN32API
 #define SOWIN_WIN32API
