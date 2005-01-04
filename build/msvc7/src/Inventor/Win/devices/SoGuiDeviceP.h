7b453ff2c9b55dfb35c92404f4e9ca8c2ec43731
Lars J. Aas <larsa@sim.no>
2005-01-04 14:01:06 +0000

    autogenerated files

---
+++ build/msvc7/src/Inventor/Win/devices/SoGuiDeviceP.h
@@ -0,0 +1,65 @@
+#ifndef SOGUI_DEVICEP_H
+#define SOGUI_DEVICEP_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#ifndef SOWIN_INTERNAL
+#error this is a private header file
+#endif /* !SOWIN_INTERNAL */
+
+#include <Inventor/Win/devices/SoWinDevice.h>
+
+// *************************************************************************
+
+class SoGuiDeviceP {
+
+public:
+  SoGuiDeviceP(SoWinDevice * p);
+  virtual ~SoGuiDeviceP();
+
+  void addEventHandler(HWND widget,
+                       SoWinEventHandler * handler,
+                       void * closure);
+  void removeEventHandler(HWND widget,
+                          SoWinEventHandler * handler,
+                          void * closure);
+
+  typedef SbBool HandlerCB(SoWinEventHandler * handler,
+                           HWND widget,
+                           MSG * event,
+                           void * handlerclosure);
+
+  void invokeHandlers(HandlerCB * cb, MSG * event);
+
+  SbPList * handlers;
+  static SbVec2s lasteventposition;
+  SbVec2s widgetsize;
+
+private:
+  class SoWinDevice * pub;
+};
+
+// *************************************************************************
+
+#endif // !SOGUI_DEVICEP_H
---
+++ build/msvc7/src/Inventor/Win/nodes/SoGuiNodes.h
@@ -0,0 +1,54 @@
+#ifndef COIN_SOGUINODES_H
+#define COIN_SOGUINODES_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#ifndef SOWIN_INTERNAL
+#error this is a private header file
+#endif /* !SOWIN_INTERNAL */
+
+#include <Inventor/Win/nodes/SoGuiSceneTexture2.h>
+#include <Inventor/Win/nodes/SoGuiViewportFix.h>
+#include <Inventor/Win/nodes/SoGuiPane.h>
+#include <Inventor/Win/nodes/SoGuiPosition.h>
+#include <Inventor/Win/nodes/SoGuiTranslation.h>
+#include <Inventor/Win/nodes/SoGuiFrame.h>
+#include <Inventor/Win/nodes/SoGuiImage.h>
+#include <Inventor/Win/nodes/SoGuiLabel.h>
+#include <Inventor/Win/nodes/SoGuiToggleButton.h>
+#include <Inventor/Win/nodes/SoGuiRadioButton.h>
+#include <Inventor/Win/nodes/SoGuiClickCounter.h>
+#include <Inventor/Win/nodes/SoGuiSlider1.h>
+#include <Inventor/Win/nodes/SoGuiSlider2.h>
+#include <Inventor/Win/nodes/SoGuiColorEditor.h>
+#include <Inventor/Win/nodes/SoGuiMaterialEditor.h>
+#include <Inventor/Win/nodes/SoGuiViewpointWrapper.h>
+
+class SoGuiNodes {
+public:
+  static void initClasses(void);
+
+};
+
+#endif // !COIN_SOGUINODES_H
