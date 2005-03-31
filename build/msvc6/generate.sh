ea99fa1322804ca0d223f51d7b03d0fe6968b982
Lars J. Aas <larsa@sim.no>
2005-03-31 16:37:02 +0000

    update/fixes

--- build/msvc6/generate.sh
+++ build/msvc6/generate.sh
@@ -4,25 +4,36 @@
 #
 # 20041214 larsa
 
-rm -rf sowin1.dsp sowin1.dsw installsowinheaders.bat
+rm -f sowin1.dsp sowin1.dsw installsowinheaders.bat
 
 ../../configure --enable-msvcdsp --with-msvcrt=mt || exit 1
-make
+make || exit 1
 
 build_pwd=`pwd`
 build="`cygpath -w $build_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
+build_pwd="`pwd | sed -e 's/\\//\\\\\\\\/g'`\\\\"
+
 source_pwd=`cd ../..; pwd`
 source="`cygpath -w $source_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
-
-# cp sowin1.dsp orig.dsp
+source_pwd="`(cd ../..; pwd) | sed -e 's/\\//\\\\\\\\/g'`"
 
 sed \
   -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
   -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
   -e 's/$/\r/g' \
   <sowin1.dsp >new.dsp
+
 mv new.dsp sowin1.dsp
 
-# How can I avoid the modal upgrade prompt-dialog for MSVC7.1 here???
-# devenv /command "File.OpenProject $build\\sowin1.dsp"
+sed \
+  -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
+  -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
+  -e 's/$/\r/g' \
+  <installsowinheaders.bat >new.bat
+
+mv new.bat installsowinheaders.bat
 
